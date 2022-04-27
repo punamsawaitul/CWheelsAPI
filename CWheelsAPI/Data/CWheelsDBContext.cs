f08521e1bf34cdaa56163571967252e9633bbce8
Punam Sawaitul <104350831+punamsawaitul@users.noreply.github.com>
2022-04-27 16:40:23 +0530

    Add files via upload

---
+++ CWheelsAPI/Data/CWheelsDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CWheelsAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CWheelsAPI.Data
+{
+    public class CWheelsDBContext: DbContext
+    {
+        public CWheelsDBContext(DbContextOptions<CWheelsDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+    }
+}
