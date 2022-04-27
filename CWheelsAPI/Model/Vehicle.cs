ba3f99a23e5a521874056dc47998d5de8743c1c1
Punam Sawaitul <104350831+punamsawaitul@users.noreply.github.com>
2022-04-27 16:41:40 +0530

    Add files via upload

---
+++ CWheelsAPI/Model/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CWheelsAPI.Model
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title cannot be empty")]
+        public string Title { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public string Color { get; set; }
+    }
+}
